e78e7a54dfa79e65e2a1806e134d4500dd33426e
whyrinarichano <129941108+whyrinarichano@users.noreply.github.com>
2023-07-20 15:26:46 +0900

    크롤링+OCR

---
+++ test/final_clova-2.py
@@ -0,0 +1,440 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver import ActionChains
+# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
+import time
+
+import urllib
+import ssl
+# 페이지 로딩을 기다리는데에 사용할 time 모듈 import
+import os
+import shutil
+
+#이미지 다운로드
+from urllib.request import urlretrieve
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import numpy as np
+import platform
+from PIL import ImageFont, ImageDraw, Image
+from matplotlib import pyplot as plt
+ 
+import uuid
+import json
+import time
+import cv2
+import requests
+import re
+
+
+# In[33]:
+
+
+def find_item(key):
+    #query_txt = input('검색할 키워드 :')
+    #query_txt=key
+    query_txt = key
+    
+    driver = webdriver.Chrome()
+    HomeUrl="https://front.homeplus.co.kr/"
+    driver.get(HomeUrl)
+
+    # 페이지가 완전히 로딩되도록 3초동안 기다림
+    time.sleep(3)
+    driver.find_element(By.NAME, "keyword").send_keys(query_txt, Keys.ENTER)
+    time.sleep(5) # sleep 안걸어주면 한줄당 3개의 상품들만 출력됨
+
+    ###### 1페이지 상품 -> 리스트에 담아오기 ######
+    name_list = [] # 상품명
+    price_list = [] # 가격
+    link_list = [] # 구매링크
+
+    prods = driver.find_elements(By.CLASS_NAME, 'unitItemBox')
+    # print(prods)
+
+    num=10
+    for prod in prods :
+        if(num==0):
+            break;
+        name_list.append(prod.find_element(By.CLASS_NAME, "css-12cdo53-defaultStyle-Typography-ellips").text)
+        price_list.append(prod.find_element(By.CLASS_NAME, "priceValue").text)
+        link_list.append(prod.find_element(By.CLASS_NAME, "productTitle").get_attribute('href'))
+        num-=1
+    return name_list,price_list,link_list
+
+
+# In[3]:
+
+
+def get_img(link_list):
+    driver = webdriver.Chrome()
+    HomeUrl=link_list.copy()
+    results = []
+    for link in HomeUrl:
+        re=[]
+        driver.get(link)
+        time.sleep(10)
+        try:
+            # 영양정보 이미지 위치로 이동
+            driver.execute_script("window.scrollTo(0, 3000)")
+            element = WebDriverWait(driver, 30).until(
+                EC.visibility_of_element_located((By.CLASS_NAME,"prodLabelImg"))
+            )
+        except:
+            print("error")
+
+        base = driver.find_element(By.CLASS_NAME,"prodDetailArea").find_elements(By.TAG_NAME,'img')
+        for img in base:
+            src_link = img.get_attribute('src')
+            re.append(src_link)
+        results.append(re)
+        print(re)
+        
+    return results
+
+
+# In[4]:
+
+
+def empy_file(path):
+    if os.path.isfile(path):
+        os.remove(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        raise ValueError("해당 경로를 확인해주세요")
+    os.mkdir(path)
+
+
+
+# In[43]:
+
+
+def get_preimg(name_list,results):
+    path_folder = "path" #저장할 이미지 파일 경로 입력(수정하기)
+    ssl._create_default_https_context = ssl._create_unverified_context
+    if not os.path.isdir(path_folder):
+        os.mkdir(path_folder)
+    i = 0
+    #상품 광고
+    for li in results:
+        print(li[-1])
+        img_link=li[-1]
+        
+        urllib.request.urlretrieve(img_link, path_folder + f'{name_list[i]}.jpg')
+        i += 1
+
+
+# In[6]:
+
+
+def img_show(title='image', img=None, figsize=(8 ,5)):
+    plt.figure(figsize=figsize)
+ 
+    if type(img) == list:
+        if type(title) == list:
+            titles = title
+        else:
+            titles = []
+ 
+            for i in range(len(img)):
+                titles.append(title)
+ 
+        for i in range(len(img)):
+            if len(img[i].shape) <= 2:
+                rgbImg = cv2.cvtColor(img[i], cv2.COLOR_GRAY2RGB)
+            else:
+                rgbImg = cv2.cvtColor(img[i], cv2.COLOR_BGR2RGB)
+ 
+            plt.subplot(1, len(img), i + 1), plt.imshow(rgbImg)
+            plt.title(titles[i])
+            plt.xticks([]), plt.yticks([])
+ 
+        plt.show()
+    else:
+        if len(img.shape) < 3:
+            rgbImg = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        else:
+            rgbImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+ 
+        plt.imshow(rgbImg)
+        plt.title(title)
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+
+# In[7]:
+
+
+def get_dirimgs(name_list,path):
+    root_dir = path # 디렉토리
+ 
+    img_path_list = []
+    possible_img_extension = ['.jpg', '.jpeg', '.JPG', '.bmp', '.png'] # 이미지 확장자들
+ 
+    for (root, dirs, files) in os.walk(root_dir):
+        if len(files) > 0:
+            for file_name in files:
+                if os.path.splitext(file_name)[1] in possible_img_extension:
+                    img_path = root + file_name
+                
+                    # 경로에서 \를 모두 /로 바꿔줘야함
+                    img_path = img_path.replace('\\', '/') # \는 \\로 나타내야함         
+                    img_path_list.append(img_path)
+    result=[]             
+    for name in name_list:
+        for p in img_path_list:
+            if(p.find(name)!=-1):
+                    result.append(p)
+                    break
+    return result
+
+
+# In[41]:
+
+
+def naver_clova(paths):
+    api_url = "api_url" #수정하기
+    secret_key = "secret_key" #수정하기
+    path = paths
+    re=[]
+    for p in path:
+        print(p)
+        
+        files = [('file', open(p,'rb'))]
+        request_json = {'images': [{'format': 'jpg',
+                                    'name': 'demo'
+                                   }],
+                        'requestId': str(uuid.uuid4()),
+                        'version': 'V2',
+                        'timestamp': int(round(time.time() * 1000))
+                       }
+ 
+        payload = {'message': json.dumps(request_json).encode('UTF-8')}
+ 
+        headers = {
+          'X-OCR-SECRET': secret_key,
+        }
+ 
+        response = requests.request("POST", api_url, headers=headers, data=payload, files=files)
+        result = response.json()
+        
+        all=[]
+        for field in result['images'][0]['fields']:
+            text = field['inferText']
+            all.append(text)
+#        print(all)
+        re.append(all)
+    return re
+
+
+# In[9]:
+
+
+def replace_string(all):
+    specialchars="{}[](),/:을"
+    
+    for i in specialchars:
+        #print(i)
+        for a in range(len(all)):
+            if(all[a]==i):
+                all=all.replace(i,' ')
+    return all
+
+
+# In[19]:
+
+
+def string_pre(all):
+    new_all=[]
+    for a in range(len(all)):
+        #특수문자 처리
+        all[a]=replace_string(all[a])
+
+        #포함제거
+        poham=all[a].find("포함")
+        if(poham!=-1):
+            all[a]=''
+        
+        split_point=0
+        sp_str=all[a].split(" ")
+        split_point+=len(sp_str)
+
+        for i in range(len(sp_str)):
+            new_all.append(sp_str[i])
+        
+    new_all = list(filter(None, new_all))
+    
+    return new_all
+
+
+# In[11]:
+
+
+def find_fac(all):
+    fac_keyword=['사용','제품','같은','시설','제조']
+    fac_index=[]
+    for a in range(len(all)):
+        for f in fac_keyword:
+            if(all[a].find(f)!=-1):
+                fac_index.append(a)
+    fac_index.sort()
+    return fac_index
+                
+    
+
+
+# In[20]:
+
+
+def find_facnum(fac_index):
+    f=0
+    point=0
+    for i in range(len(fac_index)-1):
+        if(fac_index[i+1]-fac_index[i]<2):
+            if(point==0):
+                f=fac_index[i]
+            point+=1
+            if(point>=3):
+                return f
+        else:
+            f=0
+            point=0
+    return -1
+        
+        
+    
+
+
+# In[21]:
+
+
+def find_index(all,keyword):
+    index=[]
+    keys=[]
+    for a in range(len(all)):
+        for k in keyword:
+            num = all[a].find(k)    
+            if(num!=-1):    
+                all[a]=k
+                index.append(a)
+                break
+    index.sort()
+    return index
+
+
+# In[23]:
+
+
+def remove_fac(pre_re,food_index,fac_num):
+    if(fac_num==-1):
+        new_food_index=[]
+        new_food_index.append(food_index)
+        return new_food_index
+    food_index.append(fac_num)
+    food_index.sort()
+    f=fac_num
+    fac=food_index.index(fac_num)
+    for i in reversed(food_index[:fac]):
+        if(f-i<=2):
+            f=i
+        else:
+            break
+
+    new_food_index=[]
+    new_food_index.append(food_index[:food_index.index(f)])
+
+    if(len(food_index)-1>food_index.index(fac_num)):
+        new_food_index.append(food_index[food_index.index(fac_num):])
+    return new_food_index
+    
+        
+    
+
+
+# In[39]:
+
+
+def stt_string(name,pre_re,allergy):
+    stt=""
+    print(f"<{name}>의 알러지유발성분 입니다.")
+    set_allergy=[]
+    for a in allergy:
+        for i in a:
+            set_allergy.append(pre_re[i])
+    result=set(set_allergy)
+    for a in result:
+        stt+=a
+        stt+=" "
+        print(a,end=' ')
+    print("입니다.")
+    return stt
+        
+
+
+# In[37]:
+
+
+def find_allergy(name_list,re):
+    keyword= ['메밀','밀','대두','복숭아','토마토'\
+            ,'우유','치즈','굴','가리비','전복','홍합','땅콩','계란','달걀'\
+            ,'고등어','멸치','명태','가자미','명태','장어','대구','참치','연어'\
+            ,'랍스터','오징어','게','새우','양고기','소고기','돼지고기','아황산포함식품'\
+            ,'번데기','닭고기','쇠고기','오징어','잣','오이','토마토','당근'\
+            ,'셀러리','감자','마늘','양파','딸기','키위','망고','바나나','감귤'\
+            ,'사과','복숭아','밤','보리','옥수수','쌀','밀가루','참깨','땅콩','콩'\
+            ,'헤이즐럿','호두','카카오','아모든','해바라기씨','CCD항원','효모'\
+            ,'올리브','아카시아','쑥'
+           '난류','알류','견과류','육류','갑각류','조개류','아황산류']
+    ite=0
+    stts=[]
+    for i in re:
+        print(f"--------------{name_list[ite]}-----------------")
+        pre_re=string_pre(i)
+        food_index=find_index(pre_re,keyword)
+        fac_index=find_fac(pre_re)
+        fac_num=find_facnum(fac_index)
+        allergy=remove_fac(pre_re,food_index,fac_num)
+        stt=stt_string(name_list[ite],pre_re,allergy)
+        stts.append(stt)
+        ite+=1
+    return stts
+
+
+# In[ ]:
+
+
+name_list,price_list,link_list = find_item('라면') #검색어 수정하기
+links=get_img(link_list)
+
+
+
+# In[29]:
+
+
+dir_path = "/Users/leechanho8511/PycharmProjects/Sogong/imgsfile/"
+empy_file(dir_path)
+get_preimg(name_list,links)
+
+
+# In[30]:
+
+
+img_path=get_dirimgs(name_list,dir_path)
+re=naver_clova(img_path)
+
+
+# In[40]:
+
+
+stts=find_allergy(name_list,re)
+print(stts)
+
